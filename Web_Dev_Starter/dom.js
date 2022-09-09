abf04a5afe6582c170d936e1b88199bc270e818f
leon2912 <falcaoleon123@gmail.com>
2022-09-09 12:52:31 +0530

    Get Elements By Class

--- Web_Dev_Starter/dom.js
+++ Web_Dev_Starter/dom.js
@@ -1,4 +1,10 @@
-console.dir(document);
+let items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[0].textContent = 'hello';
+items[2].style.backgroundColor = 'green';
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
 // console.log("DOM Manipualtion Script");
 
 // console.log(document.forms[0]);
