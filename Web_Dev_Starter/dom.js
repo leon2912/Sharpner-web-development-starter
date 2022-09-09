64a73bfd503c20ee8a72ce520ab0880b179c34bd
leon2912 <falcaoleon123@gmail.com>
2022-09-09 14:20:49 +0530

    Query Selector

--- Web_Dev_Starter/dom.js
+++ Web_Dev_Starter/dom.js
@@ -5,12 +5,23 @@
 // for(let i=0;i<items.length;i++){
 //     items[i].style.fontWeight = 'bold';
 // }
-let li = document.getElementsByTagName('li');
-console.log(items);
-li[2].style.backgroundColor = 'green';
-for(let i=0;i<li.length;i++){
-    li[i].style.fontWeight = 'bold';
-}
+// let li = document.getElementsByTagName('li');
+// console.log(items);
+// li[2].style.backgroundColor = 'green';
+// for(let i=0;i<li.length;i++){
+//     li[i].style.fontWeight = 'bold';
+// }
+
+let item1 = document.querySelector('.list-group-item:nth-child(1)');
+item1.style.backgroundColor = 'green';
+let item2 = document.querySelector('.list-group-item:nth-child(2)');
+item2.remove();
+let odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i=0;i<odd.length;i++){
+     odd[i].style.backgroundColor = 'green';
+ }
+
+
 // console.log("DOM Manipualtion Script");
 
 // console.log(document.forms[0]);
