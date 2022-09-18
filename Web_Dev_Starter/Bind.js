8a453191a9d38b79e4fdc9c61153f1e17c208700
leon2912 <falcaoleon123@gmail.com>
2022-09-18 21:33:52 +0530

    call Apply Bind

---
+++ Web_Dev_Starter/Bind.js
@@ -0,0 +1,20 @@
+let obj = {num:5};
+function addNums(a,b,c){
+    console.log(this.num + a + b + c);
+}
+let arr=[1,2,3];
+addNums.call(obj,1,2,3);
+addNums.apply(obj ,arr);
+
+let bound = addNums.bind(obj);
+bound(1,2,3);
+
+let student1 = {age:22};
+let student2 = {age:25};
+
+function printAge()
+{
+    console.log(this.age);
+}
+
+printAge.call(student2);
