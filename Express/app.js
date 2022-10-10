5e446c4ba10585b1077b1d4bf661136c96a38747
leon2912 <falcaoleon123@gmail.com>
2022-10-10 17:28:20 +0530

    Body Parser

--- Express/app.js
+++ Express/app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
-app.use((req,res,next)=>{
-    console.log("I am in middle ware 1");
-    next();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended : false}));
+
+app.use('/add-product',(req,res,next)=>{
+    console.log("Add product");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"> <input type="text" name="quantity" > <button type="submit">SUBMIT</Button></form>')
 })
 
-app.use((req,res,next)=>{
-    console.log("I am in middle ware 2");
-    res.send('<h1>Welcome to my Express App</h1>');
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/"); 
+})
+
+app.use('/',(req,res,next)=>{
+    console.log("Home Directory");
+    res.send('<h1>This is Home Page</h1>')   
 })
 
 app.listen(4000);
