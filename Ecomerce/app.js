290d10d5038521ea496327c1bc1dc4b92fb17b35
leon2912 <falcaoleon123@gmail.com>
2022-10-18 09:50:14 +0530

    product and cart relationship

--- Ecomerce/app.js
+++ Ecomerce/app.js
@@ -4,9 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -16,44 +18,50 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const sequelize = require('./util/database')
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next)=>{
-    User.findByPk(1)
-    .then((user)=>{
-        req.user = user;
-        next();
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
     })
-    .catch((err)=>{console.log(err)});
-})
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User);
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
-sequelize.sync()
-.then((result)=>{
-    return User.findByPk(1); 
-})
-.then((user)=>{
-    if(!user)
-    {
-        return User.create({ name: 'leon', email:'test@gmail.com'})
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findById(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
     }
     return user;
-})
-.then(user=>{
+  })
+  .then(user => {
     // console.log(user);
-    console.log('table created');
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
-})
-.catch((err)=>{console.log(err)});
-
-
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- Ecomerce/models/product.js
+++ Ecomerce/models/product.js
@@ -1,60 +1,26 @@
-// const { product } = require('../../Express/controllers/product');
-// const db = require('../util/database');
-
-// module.exports = class Product {
-//   constructor(title, imageUrl, description, price) {
-//     this.title = title;
-//     this.imageUrl = imageUrl;
-//     this.description = description;
-//     this.price = price;
-//   }
-
-//   save() {
-//     // const insertQuery = `INSERT INTO products ('title', 'price', 'description', 'imageUrl') VALUES ('${this.title}', '${this.price}', '${this.description}', '${this.imageUrl}');`
-//     return db.execute('INSERT INTO products ( title, price, description, imageUrl ) VALUES (?,?,?,?)',
-//     [this.title, this.price, this.description, this.imageUrl]
-//     );
-//   }
-
-//   static fetchAll() {
-//     return db.execute('select * from products');
-//   }
-
-//   static findById(prodId) {
-//     return db.execute('select * from products where id = ?',[prodId]);
-//   }
-
-//   static delete(prodId){
-//     return db.execute('delete from products where id = ?',[prodId]);
-// }
-// };
-
 const Sequelize = require('sequelize');
-const { product } = require('../../Express/controllers/product');
-const sequelize = require('../util/database')
 
-const Product = sequelize.define('product',{
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true
   },
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
+  title: Sequelize.STRING,
   price: {
     type: Sequelize.DOUBLE,
-    allowNull: false,
+    allowNull: false
   },
   imageUrl: {
     type: Sequelize.STRING,
-    allowNull: false,
+    allowNull: false
   },
   description: {
     type: Sequelize.STRING,
-    allowNull: false,
+    allowNull: false
   }
 });
 
