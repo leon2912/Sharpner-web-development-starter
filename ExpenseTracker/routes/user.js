2921e6c71a77f8fec3d6456fbb900af4f2302cc1
leon2912 <falcaoleon123@gmail.com>
2022-10-29 17:23:28 +0530

    Expense Tracker Signup with unique validation

--- ExpenseTracker/routes/user.js
+++ ExpenseTracker/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 
-router.post('signup',userController.addUser);
+router.post('/signup',userController.addUser);
 // router.delete('/:expenseid',ExpenseController.deleteExpense);
 // router.put('/:expenseid',ExpenseController.updateExpense);
 // router.get('/:expenseid',ExpenseController.getExpense);
