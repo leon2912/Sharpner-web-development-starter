93ec38c83f87e540f32aa2317b92e23bd4c32bec
leon2912 <falcaoleon123@gmail.com>
2022-10-31 09:56:33 +0530

    Expense Tracker login page Both FE and Backend

--- ExpenseTracker/controllers/user.js
+++ ExpenseTracker/controllers/user.js
@@ -1,15 +1,41 @@
 const User = require('../models/User');
 
 exports.addUser = async (req, res, next) => {
-    try{
-    console.log("Inside Create user")
-    const name =  req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    let user = await User.create({ name: name, email: email, password: password})
-    res.status(200).json({user:user,sucess:true});
+    try {
+        console.log("Inside Create user")
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
+        let user = await User.create({ name: name, email: email, password: password })
+        res.status(200).json({ user: user, sucess: true });
     }
-    catch(err){
-      res.status(403).json(err.errors[0]);
+    catch (err) {
+        res.status(403).json(err.errors[0]);
     }
-  };
+};
+
+exports.checkUser = async (req, res, next) => {
+    try {
+        console.log("Inside login user")
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
+        let user = await User.findAll({ where: { email: email } });
+        if (user.length == 0) {
+            res.status(404).json({ message: 'User Not Found', sucess: false });
+        }
+        else {
+            if (user[0].password === password) {
+                res.status(200).json({ user: user, sucess: true });
+                
+            }
+            else {
+                res.status(403).json({ message: 'Invalid Password', sucess: false });
+            }
+        }
+    }
+    catch (err) {
+        console.log(err)
+        res.status(403).json(err);
+    }
+};
---
+++ MiniProject1/login.js
@@ -0,0 +1,32 @@
+let form = document.getElementById('loginForm');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let msgContainer = document.getElementById('message');
+
+class user {
+    constructor(name, email, password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+
+form.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    let message;
+    try{
+    let newUSer = new user('leon', email.value, password.value);
+    let response = await axios.post('http://localhost:3000/user/login', newUSer)
+    window.alert('User Logged in Successfully')
+    }
+    catch(err){
+        console.log(err.response);
+        message = err.response.data.message;
+        msgContainer.innerText = message;   
+    }
+    setTimeout(()=>{
+        msgContainer.innerText = '';    
+    },10000);
+    // form.reset();
+})
