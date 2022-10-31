67b7c9d3e008499317c84363fc54aed7bed85d44
leon2912 <falcaoleon123@gmail.com>
2022-10-31 11:41:42 +0530

    encrypt passwords

--- ExpenseTracker/controllers/user.js
+++ ExpenseTracker/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.addUser = async (req, res, next) => {
     try {
@@ -6,7 +7,9 @@ exports.addUser = async (req, res, next) => {
         const name = req.body.name;
         const email = req.body.email;
         const password = req.body.password;
-        let user = await User.create({ name: name, email: email, password: password })
+        const hashedPassword = await bcrypt.hash(password,10);
+        console.log(hashedPassword);
+        let user = await User.create({ name: name, email: email, password: hashedPassword })
         res.status(200).json({ user: user, sucess: true });
     }
     catch (err) {
@@ -25,12 +28,14 @@ exports.checkUser = async (req, res, next) => {
             res.status(404).json({ message: 'User Not Found', sucess: false });
         }
         else {
-            if (user[0].password === password) {
+            let passCorrect = await bcrypt.compare(password,user[0].password);
+            console.log(passCorrect);
+            if (passCorrect) {
                 res.status(200).json({ user: user, sucess: true });
-                
+
             }
             else {
-                res.status(403).json({ message: 'Invalid Password', sucess: false });
+                res.status(401).json({ message: 'Invalid Password', sucess: false });
             }
         }
     }
