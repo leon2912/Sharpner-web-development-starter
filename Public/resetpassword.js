09f6d779da9e80bbeac3c7ee3dbad2dd709e7182
leon2912 <falcaoleon123@gmail.com>
2022-11-07 10:12:02 +0530

    Reset Password

---
+++ Public/resetpassword.js
@@ -0,0 +1,22 @@
+let form = document.getElementById('resetForm');
+let email = document.getElementById('email');
+
+form.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    console.log(email.value);
+    try{
+        let response = await axios.post('http://localhost:3000/password/forgotpassword', {email:email.value});
+        if(response.data.sucess){
+        let resetLink = response.data.resetLink;
+        form.innerHTML += `<a href="${resetLink}">Reset password</a>`
+        }
+        else{
+            form.innerHTML += `User Not Found`   
+        }
+        console.log(response);
+    }
+    catch(err){
+        console.log(err);
+        console.log(err.message.value);
+    }
+})
