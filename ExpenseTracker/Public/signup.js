d73082f306d20857b6b2f8b7af45fe4c3404d05b
leon2912 <falcaoleon123@gmail.com>
2022-12-22 13:46:55 +0530

    code written back

--- ExpenseTracker/Public/signup.js
+++ ExpenseTracker/Public/signup.js
@@ -1,32 +1,32 @@
-// let form = document.getElementById('signupForm');
-// let fname = document.getElementById('name');
-// let email = document.getElementById('email');
-// let password = document.getElementById('password');
-// let msgContainer = document.getElementById('message');
+let form = document.getElementById('signupForm');
+let fname = document.getElementById('name');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let msgContainer = document.getElementById('message');
 
-// class user {
-//     constructor(name, email, password) {
-//         this.name = name;
-//         this.email = email;
-//         this.password = password;
-//     }
-// }
+class user {
+    constructor(name, email, password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
 
-// form.addEventListener('submit', async (e)=>{
-//     e.preventDefault();
-//     let message;
-//     try{
-//     let newUSer = new user(fname.value, email.value, password.value);
-//     await axios.post('http://44.212.63.93:3000/user/signup', newUSer)
-//         message = 'User Created Successfully'
-//     }
-//     catch(err){
-//         message = err.response.data.message;   
-//     }
-//     msgContainer.innerText = message;
-//     setTimeout(()=>{
-//         msgContainer.innerText = '';    
-//     },4000);
-//     form.reset();
-// })
+form.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    let message;
+    try{
+    let newUSer = new user(fname.value, email.value, password.value);
+    await axios.post('http://44.212.63.93:3000/user/signup', newUSer)
+        message = 'User Created Successfully'
+    }
+    catch(err){
+        message = err.response.data.message;   
+    }
+    msgContainer.innerText = message;
+    setTimeout(()=>{
+        msgContainer.innerText = '';    
+    },4000);
+    form.reset();
+})
 
