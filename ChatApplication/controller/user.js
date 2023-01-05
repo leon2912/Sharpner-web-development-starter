59d54b4414d2dffc68489d9947f3a5235975082f
leon2912 <falcaoleon123@gmail.com>
2023-01-05 11:28:57 +0530

    login frontend

--- ChatApplication/controller/user.js
+++ ChatApplication/controller/user.js
@@ -5,13 +5,10 @@ const bcrypt = require('bcrypt');
 //Creating new user via signup
 exports.addUser = async (req, res, next) => {
     try {
-        console.log("Inside Create user")
-        console.log(req.body);
         const name = req.body.name;
         const email = req.body.email;
         const phone = req.body.phone;
         const password = req.body.password;
-        console.log(password);
         const hashedPassword = await bcrypt.hash(password,10);
         console.log(hashedPassword);
         let user = await User.create({ name: name, email: email,phone:phone, password: hashedPassword })
@@ -19,6 +16,6 @@ exports.addUser = async (req, res, next) => {
     }
     catch (err) {
         console.log(err);
-        res.status(403).json(err);
+        res.status(403).json(err.errors[0]);
     }
 };
---
+++ ChatApplication/public/login.js
@@ -0,0 +1,38 @@
+let form = document.getElementById('loginForm');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let msgContainer = document.getElementById('message');
+
+class user {
+    constructor(name, email, password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+
+form.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    let message;
+    try{
+    let newUSer = new user('leon', email.value, password.value);
+    let response = await axios.post('http://localhost:3000/user/login', newUSer)
+    localStorage.setItem('userToken',response.data.token);
+    document.location.href = 'expense.html';
+    }
+    catch(err){
+        console.log(err);
+        message = err.response.data.message;
+        msgContainer.innerText = message;   
+    }
+    setTimeout(()=>{
+        msgContainer.innerText = '';    
+    },10000);
+})
+
+let forgotPassword = document.getElementById('forgot-password');
+forgotPassword.addEventListener('click',(e)=>{
+    console.log(e.target);
+    window.location.replace('http://127.0.0.1:5500/ExpenseTracker/Public/reset.html');
+})
